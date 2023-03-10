33ef06c770d7241062b5792f00f1482c4d6d4adf
AbdullinSaid <93096473+AbdullinSaid@users.noreply.github.com>
2023-03-10 23:16:03 +0300

    Update client.c

--- client.c
+++ client.c
@@ -9,42 +9,35 @@
 
 size_t BUF_SIZE = 32;
 
-int main ()
-{
-    char memn[] = "shared-memory"; // object name
+int main() {
+    char memn[] = "shared-memory"; //shared memory name
     char *addr;
     int shm;
     srand(time(0));
     int mem_size = 100;
 
-    if ((shm = shm_open(memn, O_CREAT|O_RDWR, 0666)) == -1 )
-    {
+    if ((shm = shm_open(memn, O_CREAT|O_RDWR, 0666)) == -1) {
         perror("shm_open");
         return 1;
     }
-    else
-    {
+    else {
         printf("Object is open: name = %s, id = 0x%x\n", memn, shm);
     }
-    if (ftruncate(shm, mem_size) == -1)
-    {
+    if (ftruncate(shm, mem_size) == -1) {
         perror("ftruncate");
         return 1;
     }
-    else
-    {
+    else {
         printf("Memory size set and = %d\n", mem_size);
     }
     char buf[BUF_SIZE];
 
     addr = mmap(0, mem_size, PROT_WRITE|PROT_READ, MAP_SHARED, shm, 0);
-    if (addr == (char*) -1)
-    {
+    if (addr == (char*) -1) {
         perror("mmap");
         return 1;
     }
-    for (size_t i = 0; i < BUF_SIZE; ++i)
-    {
+    for (size_t i = 0; i < BUF_SIZE; ++i) {
         buf[i] = '0' + rand() % 10;
     }
     memcpy(addr, buf, sizeof(buf));
